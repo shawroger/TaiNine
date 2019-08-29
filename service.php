1650635f037c3db534d317a6456dbe09248cd0cc
Roger <47489692+shawroger@users.noreply.github.com>
2019-08-29 09:37:20 +0800

    示例接口

--- service.php
+++ service.php
@@ -1,47 +1,41 @@
 <?php
-include 'TaiNine.php';
 
-$data=isset($_POST['data']) ? htmlspecialchars($_POST['data']) : '';
-$request=isset($_POST['request']) ? htmlspecialchars($_POST['request']) : '';
-$method=isset($_POST['method']) ? htmlspecialchars($_POST['method']) : '';
+include_once('TaiNine.php');
+
+$data=isset($_POST['data']) ? $_POST['data'] : '';
+$request=isset($_POST['request']) ? $_POST['request'] : '';
+$method=isset($_POST['method']) ? $_POST['method'] : '';
+$code = array('status'=>'','message'=>'missing data','data'=>'','method'=>''); 
 
 if($data==''){
-    $code = array('status'=>0,'message'=>'missing data','data'=>'','htmlData'=>'','method'=>'','encryptData'=>'','decryptData'=>''); 
+    $code['status']=0;
+    $code['message']='missing_data';
     echo json_encode($code);
     exit();
 }
-
-//missing API
-
-if($data!=''){
-    $tq=new TaiNine($data);
+else{
     if($request=='encrypt'){
-        $tq->encrypt();
-        $code = array('status'=>1,'message'=>'encrypting data','data'=>$data,'htmlData'=>$tq->toHtml($data),'method'=>$tq->method,'encryptData'=>$tq->encryptData,'decryptData'=>''); 
+        $e=new TaiNine();
+        $e->encrypt($data);
+        $code['status']=1;
+        $code['message']='encrypting_data';
+        $code['data']=$e->encryptData;
+        $code['method']=$e->method;
         echo json_encode($code);
         exit();
     }
     else if($request=='decrypt'){
-        if(strlen($method)!=91){
-            $code = array('status'=>strlen($method),'message'=>'wrong method when decrypting','data'=>$data,'htmlData'=>$tq->toHtml($data),'method'=>'','encryptData'=>'','decryptData'=>''); 
+        if(strlen($method)!=81){
+            $code['status']=0;
+            $code['message']='wrong_method';
             echo json_encode($code);
             exit();
         }
-        
-        else if(strlen($method)==91){
-            $tq->decrypt($data,$method);
-            $code = array('status'=>1,'message'=>'decrypting data','data'=>$data,'htmlData'=>$tq->toHtml($data),'method'=>'','encryptData'=>'','decryptData'=>$tq->decryptData); 
-            echo json_encode($code);
-            exit();
-        }
-        else {
-            $code = array('status'=>0,'message'=>'wrong method','data'=>$data,'htmlData'=>$tq->toHtml($data),'method'=>'','encryptData'=>'','decryptData'=>''); 
-            echo json_encode($code);
-            exit();
-        }
-    }
-    else {
-        $code = array('status'=>0,'message'=>'wrong request','method'=>'','encryptData'=>'','decryptData'=>''); 
+        $d=new TaiNine();
+        $d->decrypt($data,$method);
+        $code['status']=1;
+        $code['message']='decrypting_data';
+        $code['data']=$d->decryptData;
         echo json_encode($code);
         exit();
     }
